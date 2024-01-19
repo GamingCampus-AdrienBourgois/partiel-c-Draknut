51160292f69e78b630bd4c478918c5e4c2f2a482
Draknut <victor-elisee.lopez@outlook.fr>
2024-01-19 14:34:04 +0100

    Por favor

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,4 +1,9 @@
 #include "Solution3.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <iostream>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -11,13 +16,24 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 
 void Solution3::SortWords()
 {
-    
-    for (auto& word : words) {
-        std::transform(word.begin(), word.end(), word.begin(),
-            [](unsigned char c) { return std::tolower(c); });
+    std::vector<std::pair<std::string, int>> wordPairs;
+    for (int i = 0; i < words.size(); ++i) {
+        wordPairs.push_back({ words[i], i });
     }
 
-    std::sort(words.begin(), words.end());
+    std::sort(wordPairs.begin(), wordPairs.end(),
+        [](const auto& a, const auto& b) {
+            return std::lexicographical_compare(a.first.begin(), a.first.end(), b.first.begin(), b.first.end(),
+            [](unsigned char c1, unsigned char c2) {
+                    return std::tolower(c1) < std::tolower(c2);
+                });
+        });
+
+    std::vector<std::string> sortedWords(words.size());
+    for (int i = 0; i < wordPairs.size(); ++i) {
+        sortedWords[i] = words[wordPairs[i].second];
+    }
+    words = sortedWords;
 }
 
 std::vector<std::string> Solution3::GetSortedWords() const
